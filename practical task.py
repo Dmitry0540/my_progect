ad165e1ea5d6ab1aa026abef8cef335f804b1745
Dmitry0540 <dima0540@gmail.com>
2024-09-17 18:39:27 +0300

    Add files via upload

---
+++ practical task.py
@@ -0,0 +1,6 @@
+the_number_of_completed= 12
+Number_of_hours_spent= 1.5
+Course_name= 'Python'
+Time_for_one_task= Number_of_hours_spent / the_number_of_completed
+print('Курс: '+ Course_name +', ' 'всего задач: ' + (str(the_number_of_completed))+ ', затрачено часов: '+(str(Number_of_hours_spent))+ ', '+ 'среднее время выполнения ' + (str(Time_for_one_task)),"часов")
+
